d73685aa9b2407329e678b9708945092029c19c9
shyowon26 <139289180+shyowon26@users.noreply.github.com>
2023-07-14 14:04:37 +0900

    Add files via upload

--- skintpl/js/banner.js
+++ skintpl/js/banner.js
@@ -1,73 +1,73 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-    $(document).on('click', '.btnRb .tit span,.rbPop .close,.mobOpen', function() {
-        $('body').toggleClass('rbActive');
-        if ($('body').hasClass('rbActive')) {
+	$(document).on('click','.btnRb .tit span,.rbPop .close,.mobOpen',function(){
+		$('body').toggleClass('rbActive');
+        if($('body').hasClass('rbActive')){
             thumbTbSlide.autoplay.stop();
-        } else {
+        }else{
             thumbTbSlide.autoplay.start();
         }
-    })
+	})
 
-    if ($('.thumbTbSlide li').length > 1) {
-        thumbTbSlide = new Swiper('.thumbTbSlide .swiper-container', {
-            slidesPerView: 'auto',
-            spaceBetween: 0,
-            loop: true,
-            navigation: {
-                nextEl: '.rbThumbNext',
-                prevEl: '.rbThumbPrev',
-            },
-            observer: true,
-            observeParents: true,
+	if($('.thumbTbSlide li').length > 1){
+		thumbTbSlide = new Swiper('.thumbTbSlide .swiper-container', {
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			loop: true,
+			 navigation: {
+				nextEl: '.rbThumbNext',
+				prevEl: '.rbThumbPrev',
+			  },
+			observer: true, 
+			observeParents: true,
             autoplay: {
                 disableOnInteraction: false,
                 pauseOnMouseEnter: true,
             },
-
-        });
-
-
-        rbSwiper = new Swiper('.rbSlide .swiper-container', {
-            slidesPerView: 'auto',
-            spaceBetween: 0,
-            loop: true,
-            navigation: {
-                nextEl: '.rbNext',
-                prevEl: '.rbPrev',
-            },
-            observer: true,
-            observeParents: true,
-        });
-
-        $('.rbWrap .numTotal').text($('.thumbTbSlide .swiper-slide:not(.swiper-slide-duplicate)').length)
-
-        thumbTbSlide.on('slideChange', function(e) {
-            $('.rbWrap .chNum').text(Number(thumbTbSlide.slides.eq(thumbTbSlide.activeIndex).attr('data-swiper-slide-index')) + 1);
-            rbSwiper.slideTo(thumbTbSlide.activeIndex)
-        });
-
-        rbSwiper.on('slideChange', function(a, b, c) {
-            thumbTbSlide.slideTo(rbSwiper.activeIndex)
-        });
+            
+		});
 
 
-    } else {
-        $('.rbWrap').addClass('arrowNone');
-    }
+		rbSwiper = new Swiper('.rbSlide .swiper-container', {
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			loop: true,
+			 navigation: {
+				nextEl: '.rbNext',
+				prevEl: '.rbPrev',
+			  },
+			observer: true, 
+			observeParents: true,
+		});
+
+		$('.rbWrap .numTotal').text($('.thumbTbSlide .swiper-slide:not(.swiper-slide-duplicate)').length)
+
+		thumbTbSlide.on('slideChange', function (e) {
+			$('.rbWrap .chNum').text(Number(thumbTbSlide.slides.eq(thumbTbSlide.activeIndex).attr('data-swiper-slide-index')) + 1);
+			rbSwiper.slideTo(thumbTbSlide.activeIndex)
+		});
+
+		rbSwiper.on('slideChange', function (a,b,c) {
+			thumbTbSlide.slideTo(rbSwiper.activeIndex)
+		});
 
 
+	}else{
+		$('.rbWrap').addClass('arrowNone');
+	}
+
+	
 });
 
-$(window).load(function() {
+$(window).load(function(){
 
 
 
-    if ($(window).width() > 1025 && $(".mCustom").length > 0) {
-        var mCustom = $(".mCustom").mCustomScrollbar({
-            theme: "minimal"
-        });
-    }
+	if($(window).width() > 1025 && $(".mCustom").length > 0){
+		var mCustom = $(".mCustom").mCustomScrollbar({
+			theme:"minimal"
+		});
+	}
 
 
 })
